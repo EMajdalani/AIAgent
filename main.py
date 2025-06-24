44a7be6b920f101f4f3d16e1870a53504f2b7213
EMajdalani <EMajdalani121@gmail.com>
2025-06-24 15:45:08 -0400

    Added verbose command

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
-
     client = genai.Client(api_key=api_key)
 
+    if len(sys.argv) < 2:
+        print("Please provide a prompt as a command line argument.")
+        exit(1)
+        return
+    
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=sys.argv[1])])
+    ]
+    
     response = client.models.generate_content(
         model = "gemini-2.0-flash-001",
-        contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents = messages
     )
 
     metadata = response.usage_metadata
@@ -19,7 +29,12 @@ def main():
 
 
     print(response.text)
-    print(f"Prompt tokens: {prompt_tokens}\nResponse tokens: {response_tokens}")
+
+    if len(sys.argv) > 2:
+        if sys.argv[2] == "--verbose":
+            print(f"User prompt: {sys.argv[1]}")
+            print(f"Prompt tokens: {prompt_tokens}")
+            print(f"Response tokens: {response_tokens}")
 
     return
 
